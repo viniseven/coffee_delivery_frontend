a9d44bfaf129480d01cdd0afad06eb96bc0fcafd
Marcus Vinicius <marcusv892@gmail.com>
2026-01-04 15:35:49 -0300

    feat: containerTitle component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ContainerTitle from "./components/ContainerTitle"
 import Header from "./components/Header"
 
 function App() {
 	return (
 		<main>
 			<Header />
+			<ContainerTitle />
 		</main>
 	)
 }
