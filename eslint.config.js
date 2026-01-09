862f4adf85511919b697f5609266b79ae2e13a6e
Marcus Vinicius <marcusv892@gmail.com>
2026-01-08 22:46:54 -0300

    feat: added plugin import sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,35 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import { defineConfig, globalIgnores } from "eslint/config"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-    },
-  },
+	globalIgnores(["dist"]),
+	{
+		files: ["**/*.{js,jsx}"],
+		extends: [
+			js.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				ecmaFeatures: { jsx: true },
+				sourceType: "module",
+			},
+		},
+		rules: {
+			"no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+			"simple-import-sort/imports": "error",
+			"simple-import-sort/exports": "error",
+		},
+		plugins: {
+			"simple-import-sort": simpleImportSort,
+		},
+	},
 ])
