eda13f10bc3278096ded569b654faef079fb196c
Marcus Vinicius <marcusv892@gmail.com>
2026-01-22 18:01:22 -0300

    feat: create context and interface ICartContext

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,25 +1,36 @@
 import CartProduct from "@/types/CartTypes"
-import { createContext, FunctionComponent, ReactNode, useState } from "react"
+import ProductTypes from "@/types/ProductTypes"
+import { createContext, ReactNode, useState } from "react"
+
+interface ICartContext {
+	products: CartProduct[]
+	addProductToCart: (product: ProductTypes) => void
+	removeProductToCart: (prouct: ProductTypes) => void
+}
+
+export const CartContext = createContext<ICartContext>({} as ICartContext)
 
 interface CartProviderProps {
 	children: ReactNode
 }
 
-interface ICartContext {
-	products: CartProduct[]
-}
-
-const CartContext = createContext<ICartContext>({
-	products: [],
-})
-
-const CartContextProvider: FunctionComponent<CartProviderProps> = ({
-	children,
-}) => {
+function CartContextProvider({ children }: CartProviderProps) {
 	const [products, setProducts] = useState<CartProduct[]>([])
 
+	function addProductToCart(product: ProductTypes) {
+		setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
+	}
+
+	function removeProductToCart(product: ProductTypes) {
+		setProducts((products) => products.filter((item) => item.id != product.id))
+	}
+
 	return (
-		<CartContext.Provider value={{ products }}>{children}</CartContext.Provider>
+		<CartContext.Provider
+			value={{ products, addProductToCart, removeProductToCart }}
+		>
+			{children}
+		</CartContext.Provider>
 	)
 }
 
