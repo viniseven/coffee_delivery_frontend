dbcbde5cd0c69facb4862edf85cf5989e2f1286d
Marcus Vinicius <marcusv892@gmail.com>
2026-01-21 22:01:21 -0300

    feat: added count quantity card in icon cart button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,8 +15,11 @@ function Header() {
 					<User className="text-purple fill-purple" />
 					<p className="text-purple">Marcus Vinícius</p>
 				</Button>
-				<Link to="/checkoutpayment">
-					<Button className="bg-yellow-light">
+				<Link to="/checkoutpayment" className="">
+					<Button className="bg-yellow-light relative">
+						<div className="bg-yellow-dark absolute bottom-5 left-9 flex h-5 w-5 items-center justify-center rounded-full p-0.5">
+							<span className="text-lg text-white">1</span>
+						</div>
 						<ShoppingCart className="text-yellow-dark fill-yellow-dark" />
 					</Button>
 				</Link>
