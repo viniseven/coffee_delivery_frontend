2251fd7a4b5260a221dc43fb5640d54e2f996d06
Marcus Vinicius <marcusv892@gmail.com>
2026-01-22 18:00:01 -0300

    feat: add context and functions handleRemoveProduct

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,27 +1,35 @@
-import Coffee from "../assets/CoffeeTradicional.svg"
 import AddQuantityProduct from "./QuantityProduct"
-import BuyButton from "./BuyButton"
 import RemoveProductButton from "./RemoveProductButton"
 import Separator from "./Separator"
+import priceCentsConvert from "@/utils/priceCentsConverter"
+import CoffeeCardProps from "@/types/CoffeeProductProps"
+import { useContext } from "react"
+import { CartContext } from "@/contexts/CartContext"
 
-function CartItem() {
+function CartItem({ coffee }: CoffeeCardProps) {
+	const { removeProductToCart } = useContext(CartContext)
+
+	function handleRemoveProductToCart() {
+		removeProductToCart(coffee)
+	}
+
 	return (
 		<div>
 			<div className="mt-6 mb-6 flex items-center gap-5">
 				<img
-					src={Coffee}
+					src={coffee.imgUrl}
 					alt="Imagem de uma xícara com café tradicional, visto de cima"
 					className="size-auto w-20"
 				/>
 				<div className="grow">
-					<p className="text-lg">Expresso Tradicional</p>
+					<p className="text-lg">{coffee.name}</p>
 					<div className="mt-2 flex flex-col gap-1 md:flex-row md:gap-4">
 						<AddQuantityProduct />
-						<RemoveProductButton />
+						<RemoveProductButton onClick={handleRemoveProductToCart} />
 					</div>
 				</div>
 				<p className="font-bold lg:text-2xl">
-					R$ <span>9,90</span>
+					R$ <span>{priceCentsConvert(coffee.priceInCents)}</span>
 				</p>
 			</div>
 			<Separator />
