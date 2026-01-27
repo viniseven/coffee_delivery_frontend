423325b4f5596460ee079c94591e80996c930e94
Marcus Vinicius <marcusv892@gmail.com>
2026-01-26 23:13:31 -0300

    refactor: name params componente Productcard for product

--- src/components/SectionProducts.tsx
+++ src/components/SectionProducts.tsx
@@ -7,7 +7,7 @@ function SectionProducts() {
 			<h2 className="mt-20 mb-14">Nossos cafés</h2>
 			<div className="grid grid-cols-1 justify-items-center gap-10 md:grid-cols-3 lg:grid-cols-4">
 				{PRODUCTS.map((product) => (
-					<ProductCard key={product.id} coffee={product} />
+					<ProductCard key={product.id} product={product} />
 				))}
 			</div>
 		</div>
