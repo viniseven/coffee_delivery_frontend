fa40eca6164c6770b90913ef69766edd3760431a
Marcus Vinicius <marcusv892@gmail.com>
2026-01-26 23:14:10 -0300

    feat: add interface and functions

--- src/components/QuantityProduct.tsx
+++ src/components/QuantityProduct.tsx
@@ -1,26 +1,24 @@
 import { Plus, Minus } from "lucide-react"
 import { Button } from "./ui/button"
-import { useState } from "react"
 
-function QuantityProduct() {
-	const [quantity, setQuantity] = useState(1)
-
-	function handleAddQuantity() {
-		setQuantity((prevState) => prevState + 1)
-	}
-
-	function handleRemoveQuantity() {
-		setQuantity((prevState) => (prevState > 1 ? prevState - 1 : 1))
-	}
-
+interface QuantityProductProps {
+	quantity: number
+	handleAddQuantityProduct: () => void
+	handleRemoveQuantityProduct: () => void
+}
+function QuantityProduct({
+	quantity,
+	handleAddQuantityProduct,
+	handleRemoveQuantityProduct,
+}: QuantityProductProps) {
 	return (
 		<div className="flex max-w-24 flex-row gap-2">
 			<div className="bg-base-button flex items-center rounded-md">
-				<Button onClick={handleRemoveQuantity}>
+				<Button onClick={handleRemoveQuantityProduct}>
 					<Minus className="text-purple" />
 				</Button>
 				<span>{quantity}</span>
-				<Button onClick={handleAddQuantity}>
+				<Button onClick={handleAddQuantityProduct}>
 					<Plus className="text-purple" />
 				</Button>
 			</div>
