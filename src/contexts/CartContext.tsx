80b3c5abe803e41d2c7c52004bf66dff324bff41
Marcus Vinicius <marcusv892@gmail.com>
2026-01-26 23:13:06 -0300

    feat: add atributes and function in context

--- src/contexts/CartContext.tsx
+++ src/contexts/CartContext.tsx
@@ -1,11 +1,14 @@
-import CartProduct from "@/types/CartTypes"
+import CartProduct from "@/types/CartProduct"
 import ProductTypes from "@/types/ProductTypes"
 import { createContext, ReactNode, useState } from "react"
 
+//Interface do meu contexto do carrinho. Recebendo no atributo products, a interface do meu cartProducts, tenho além dos atributos de produto, também quantidade
 interface ICartContext {
 	products: CartProduct[]
-	addProductToCart: (product: ProductTypes) => void
-	removeProductToCart: (prouct: ProductTypes) => void
+	addProductToCart: (product: ProductTypes, quantity: number) => void
+	removeProductCart: (productId: string) => void
+	addQuantityProductCart: (productId: string) => void
+	removeQuantityProductCart: (productId: string) => void
 }
 
 export const CartContext = createContext<ICartContext>({} as ICartContext)
@@ -17,17 +20,46 @@ interface CartProviderProps {
 function CartContextProvider({ children }: CartProviderProps) {
 	const [products, setProducts] = useState<CartProduct[]>([])
 
-	function addProductToCart(product: ProductTypes) {
-		setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
+	function addProductToCart(product: ProductTypes, quantity: number) {
+		setProducts((prevState) => [...prevState, { ...product, quantity }])
 	}
 
-	function removeProductToCart(product: ProductTypes) {
-		setProducts((products) => products.filter((item) => item.id != product.id))
+	function removeProductCart(productId: string) {
+		setProducts((products) => products.filter((item) => item.id != productId))
 	}
 
+	function addQuantityProductCart(productId: string) {
+		setProducts((productsState) =>
+			productsState.map((productState) =>
+				productState.id == productId
+					? { ...productState, quantity: productState.quantity + 1 }
+					: productState
+			)
+		)
+	}
+
+	function removeQuantityProductCart(productId: string) {
+		setProducts((productsState) =>
+			productsState.map((productState) =>
+				productState.id == productId
+					? {
+							...productState,
+							quantity:
+								productState.quantity > 1 ? productState.quantity - 1 : 1,
+						}
+					: productState
+			)
+		)
+	}
 	return (
 		<CartContext.Provider
-			value={{ products, addProductToCart, removeProductToCart }}
+			value={{
+				products,
+				addProductToCart,
+				removeProductCart,
+				addQuantityProductCart,
+				removeQuantityProductCart,
+			}}
 		>
 			{children}
 		</CartContext.Provider>
