e52683f68a3e20dc4f224f0f6108521f5c1979e0
Marcus Vinicius <marcusv892@gmail.com>
2026-02-06 16:30:06 -0300

    refactor: ContainerCartComponent name

---
+++ src/components/ContainerCartComponent.tsx
@@ -0,0 +1,29 @@
+import { map } from "zod"
+import CartItem from "./CartItem"
+import TotalPrice from "./TotalPrice"
+import { Button } from "./ui/button"
+import { Link } from "react-router-dom"
+import { useContext } from "react"
+import { CartContext } from "@/contexts/CartContext"
+
+function ContainerCartComponent() {
+	return (
+		<div className="w-full">
+			<h3>Cafés selecionados</h3>
+			<div className="bg-base-card grid gap-9 rounded-md p-1 md:grid-cols-2 lg:p-10">
+				<div></div>
+
+				<div className="flex flex-col">
+					<TotalPrice />
+					<Link to="/orderconfirmation">
+						<Button className="bg-yellow font-content w-full items-center p-3 font-bold text-white">
+							CONFIRMAR PEDIDO
+						</Button>
+					</Link>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ContainerCartComponent
