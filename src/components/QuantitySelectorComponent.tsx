94aef119241bf4559a06c7270c087409e21c7128
Marcus Vinicius <marcusv892@gmail.com>
2026-02-06 16:20:17 -0300

    refactor: name for QuantitySelectorComponent

---
+++ src/components/QuantitySelectorComponent.tsx
@@ -0,0 +1,20 @@
+import { Plus, Minus } from "lucide-react"
+import { Button } from "./ui/button"
+
+function QuantitySelectorComponent() {
+	return (
+		<div className="flex max-w-24 flex-row gap-2">
+			<div className="bg-base-button flex items-center rounded-md">
+				<Button>
+					<Minus className="text-purple" />
+				</Button>
+				<span>1</span>
+				<Button>
+					<Plus className="text-purple" />
+				</Button>
+			</div>
+		</div>
+	)
+}
+
+export default QuantitySelectorComponent
