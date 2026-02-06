69d619693cddc87a30846de1b041483288c315ec
Marcus Vinicius <marcusv892@gmail.com>
2026-02-06 16:26:41 -0300

    refactor: ItemCartComponent name

---
+++ src/components/ItemCartComponent.tsx
@@ -0,0 +1,33 @@
+import QuantityProduct from "./QuantityProduct"
+import RemoveProductButton from "./RemoveProductButton"
+import Separator from "./Separator"
+
+function ItemCartComponent() {
+	return (
+		<div>
+			<div className="mt-6 mb-6 flex items-center gap-5">
+				<img
+					src=""
+					alt="Imagem de uma xícara com café tradicional, visto de cima"
+					className="size-auto w-20"
+				/>
+				<div className="grow">
+					<p className="text-lg">""</p>
+					<div className="mt-2 flex flex-col gap-1 md:flex-row md:gap-4">
+						<QuantityProduct {}/>
+						<RemoveProductButton />
+					</div>
+				</div>
+				<p className="font-bold lg:text-2xl">
+					R${" "}
+					<span>
+						{}
+					</span>
+				</p>
+			</div>
+			<Separator />
+		</div>
+	)
+}
+
+export default ItemCartComponent
