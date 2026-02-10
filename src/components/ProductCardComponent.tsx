3ce3ef95b2004665bc3f65bc8613626cd9053c18
Marcus Vinicius <marcusv892@gmail.com>
2026-02-10 17:01:34 -0300

    refactor: interface ProductCardProps in ProductCardComponent

--- src/components/ProductCardComponent.tsx
+++ src/components/ProductCardComponent.tsx
@@ -5,30 +5,41 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card"
-import QuantityProduct from "./QuantityProduct"
+import QuantityProduct from "./QuantitySelectorComponent"
 import BuyButton from "./BuyButtonComponent"
+import ProductTypes from "@/types/ProductTypes"
 
-function ProductCardComponent() {
+interface ProductCardProps {
+	data: ProductTypes
+}
+
+function ProductCardComponent({ data }: ProductCardProps) {
 	return (
 		<Card className="bg-base-card flex w-3xs flex-col items-center rounded-tr-[36px] rounded-bl-[36px] border-none px-2 text-center">
 			<CardHeader className="flex flex-col items-center">
 				<img
-					src={}
+					src={data.imgUrl}
 					alt="Imagem de uma xícara com café tradicional, visto de cima"
 					className="size-auto w-30"
 				/>
-				<div className="flex gap-1"></div>
+				<div className="flex gap-1">
+					{data.label.map((label) => (
+						<p className="bg-yellow-light text-yellow-dark rounded-2xl px-1 text-xs">
+							{label}
+						</p>
+					))}
+				</div>
 			</CardHeader>
 			<CardTitle>
-				<h3>{}</h3>
+				<h3>{data.name}</h3>
 			</CardTitle>
 			<CardDescription className="mb-8">
-				<p className="text-gray-light mt-2">{}</p>
+				<p className="text-gray-light mt-2">{data.description}</p>
 			</CardDescription>
 			<CardFooter className="gap-3">
 				<p className="text-base">
 					R$
-					<span className="ml-1 text-2xl font-bold"></span>
+					<span className="ml-1 text-2xl font-bold">{data.priceInCents}</span>
 				</p>
 				<QuantityProduct />
 				<BuyButton />
