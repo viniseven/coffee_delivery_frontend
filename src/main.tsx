e83aebd72e6b79188b3e18f3fea313188afeec74
Marcus Vinicius <marcusv892@gmail.com>
2026-02-11 22:54:20 -0300

    feat: add cart context in application

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App"
-import CartContextProvider from "./contexts/CartContext"
 import { BrowserRouter } from "react-router-dom"
+import CartContextProvider from "./contexts/CartContext"
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
