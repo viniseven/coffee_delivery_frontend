3429d8c3c3550a252fc773e9cde9a8f43d77c479
Marcus Vinicius <marcusv892@gmail.com>
2026-02-12 19:43:47 -0300

    feat: add interface and function handleRemove productId

--- src/components/RemoveProductButtonComponent.tsx
+++ src/components/RemoveProductButtonComponent.tsx
@@ -2,13 +2,21 @@ import { Button } from "./ui/button"
 import { Trash2 } from "lucide-react"
 import { ComponentProps } from "react"
 
-interface RemoveButtonProps extends ComponentProps<typeof Button> {}
+interface RemoveButtonProps extends ComponentProps<typeof Button> {
+	productId: string
+	handleRemoveProduct: (productId: string) => void
+}
 
-function RemoveProductButtonComponent({ ...props }: RemoveButtonProps) {
+function RemoveProductButtonComponent({
+	productId,
+	handleRemoveProduct,
+	...props
+}: RemoveButtonProps) {
 	return (
 		<Button
 			className="bg-base-button flex max-w-24 items-center rounded-md"
 			{...props}
+			onClick={() => handleRemoveProduct(productId)}
 		>
 			<Trash2 className="text-purple" />
 			<span className="text-base-text">REMOVER</span>
