3a009364b6b67459b4077288bb024dab5b1480c2
Marcus Vinicius <marcusv892@gmail.com>
2026-02-12 18:22:56 -0300

    feat: button props context function

--- src/components/BuyButtonComponent.tsx
+++ src/components/BuyButtonComponent.tsx
@@ -1,12 +1,24 @@
 import { ShoppingCart } from "lucide-react"
 import { Button } from "./ui/button"
 import { ComponentProps } from "react"
+import CartProduct from "@/types/CartProduct"
 
-interface BuyButtonProps extends ComponentProps<typeof Button> {}
+interface BuyButtonProps extends ComponentProps<typeof Button> {
+	product: CartProduct
+	handleBuyProduct: (product: CartProduct) => void
+}
 
-function BuyButtonComponent({ ...props }: BuyButtonProps) {
+function BuyButtonComponent({
+	product,
+	handleBuyProduct,
+	...props
+}: BuyButtonProps) {
 	return (
-		<Button className="bg-purple-dark text-white" {...props}>
+		<Button
+			className="bg-purple-dark text-white"
+			{...props}
+			onClick={() => handleBuyProduct(product)}
+		>
 			<ShoppingCart className="fill-white" />
 		</Button>
 	)
