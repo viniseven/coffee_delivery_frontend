fb7e3d64fc2b6aadda3aa513fe6eb343ee7ecfe2
Marcus Vinicius <marcusv892@gmail.com>
2026-02-12 20:59:41 -0300

    feat: product show in total price component

--- src/components/TotalPriceComponent.tsx
+++ src/components/TotalPriceComponent.tsx
@@ -1,17 +1,29 @@
+import { useContext } from "react"
+import { CartContext } from "@/contexts/CartContext"
+import priceCentsConvert from "@/utils/priceCentsConverter"
+
 function TotalPrice() {
+	const { productCartTotalPrice } = useContext(CartContext)
+
+	const freightValue = 350
+
 	return (
 		<div className="text-base-text font-content flex flex-col gap-3">
 			<div className="flex justify-between">
 				<span className="text-sm">Total de itens</span>
-				<span>R$ 29,70</span>
+				<span>{priceCentsConvert(productCartTotalPrice)}</span>
 			</div>
 			<div className="flex justify-between">
 				<span className="text-sm">Entrega</span>
-				<span>R$ 3,50</span>
+				<span>{priceCentsConvert(freightValue)}</span>
 			</div>
 			<div className="flex justify-between text-xl font-bold">
 				<span>Total</span>
-				<span>R$ 33,20</span>
+				<span>
+					{productCartTotalPrice > 0
+						? priceCentsConvert(productCartTotalPrice + freightValue)
+						: priceCentsConvert(0)}
+				</span>
 			</div>
 		</div>
 	)
