2317a7837d7d43b21fbe2181d94644786089f2a9
Marcus Vinicius <marcusv892@gmail.com>
2026-02-14 13:19:46 -0300

    feat: add dropdown menu

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -4,6 +4,14 @@ import { Button } from "./ui/button"
 import { ShoppingCart, User } from "lucide-react"
 import { useContext } from "react"
 import { CartContext } from "@/contexts/CartContext"
+import {
+	DropdownMenu,
+	DropdownMenuTrigger,
+	DropdownMenuContent,
+	DropdownMenuItem,
+} from "./ui/dropdown-menu"
+import ItemCartComponent from "./ItemCartComponent"
+import TotalPrice from "./TotalPriceComponent"
 
 function HeaderComponent() {
 	const { productsCart } = useContext(CartContext)
@@ -19,16 +27,32 @@ function HeaderComponent() {
 					<User className="text-purple fill-purple" />
 					<p className="text-purple">Marcus Vinícius</p>
 				</Button>
-				<Link to="/checkoutpayment" className="">
-					<Button className="bg-yellow-light relative">
-						<div className="bg-yellow-dark absolute bottom-5 left-9 flex h-4 w-4 items-center justify-center rounded-full p-0.5">
-							<span className="font-semibold text-white">
-								{productsCart.length}
-							</span>
-						</div>
-						<ShoppingCart className="text-yellow-dark fill-yellow-dark" />
-					</Button>
-				</Link>
+
+				<DropdownMenu modal={false}>
+					<DropdownMenuTrigger>
+						<Button className="bg-yellow-light relative">
+							<div className="bg-yellow-dark absolute bottom-5 left-9 flex h-4 w-4 items-center justify-center rounded-full p-0.5">
+								<span className="font-semibold text-white">
+									{productsCart.length}
+								</span>
+							</div>
+							<ShoppingCart className="text-yellow-dark fill-yellow-dark" />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent className="bg-base-card px-2">
+						{productsCart.map((data) => (
+							<DropdownMenuItem onSelect={(e) => e.preventDefault()}>
+								<ItemCartComponent product={data} />
+							</DropdownMenuItem>
+						))}
+						<TotalPrice />
+						<Link to="/checkoutpayment">
+							<Button className="bg-yellow font-content w-full items-center p-3 font-bold text-white">
+								CONFIRMAR PEDIDO
+							</Button>
+						</Link>
+					</DropdownMenuContent>
+				</DropdownMenu>
 			</div>
 		</div>
 	)
